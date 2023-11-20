1859316c3397034da113a3a8ba31adf2036fd39f
Amaro Lima <149506965+bratamaro@users.noreply.github.com>
2023-11-20 17:13:58 -0300

    Update criar-site-iis.ps1

--- criar-site-iis.ps1
+++ criar-site-iis.ps1
@@ -1,5 +1,6 @@
 # Criar um diretório para o site
-New-Item -ItemType Directory -Path 'C:\inetpub\wwwroot\webappiis'
+#New-Item -ItemType Directory -Path 'C:\inetpub\wwwroot\webappiis'
+New-Item -ItemType Directory -Path '%SystemDrive%\inetpub\wwwroot\webappiis2'
 
 # Criar uma página de exemplo
 @"
@@ -12,7 +13,9 @@ New-Item -ItemType Directory -Path 'C:\inetpub\wwwroot\webappiis'
     <p>Este é um site de exemplo no IIS criado por um pipeline do Azure DevOps.</p>
 </body>
 </html>
-"@ | Out-File -FilePath 'C:\inetpub\wwwroot\webappiis\index.html'
+#"@ | Out-File -FilePath 'C:\inetpub\wwwroot\webappiis\index.html'
+"@ | Out-File -FilePath '%SystemDrive%\inetpub\wwwroot\webappiis2\index.html'
 
 # Criar um site no IIS
-New-WebSite -Name "SiteExemplo" -PhysicalPath 'C:\inetpub\wwwroot\webappiis' -Port 80
+#New-WebSite -Name "SiteExemplo" -PhysicalPath 'C:\inetpub\wwwroot\webappiis' -Port 80
+New-WebSite -Name "SiteExemplo" -PhysicalPath '%SystemDrive%\inetpub\wwwroot\webappiis2' -Port 8082
